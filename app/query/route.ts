5bb1cd79f6b0148b166f0ef8d382b1083e731f00
PieterStrike <114082365+PieterStrike@users.noreply.github.com>
2025-04-10 16:07:57 +0700

    Update route.ts

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,9 +1,9 @@
-// import postgres from 'postgres';
+import postgres from 'postgres';
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 async function listInvoices() {
-	const data = await sql`
+	// const data = await sql`
     SELECT invoices.amount, customers.name
     FROM invoices
     JOIN customers ON invoices.customer_id = customers.id
